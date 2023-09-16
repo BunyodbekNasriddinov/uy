2320fdb9e4fd81ad25fc3fe9600d3aec5f4862dd
BunyodbekNasriddinov <nasriddinoffbunyodbek@gmail.com>
2023-09-17 00:45:23 +0500

    Update app.js

--- app.js
+++ app.js
@@ -23,6 +23,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.use(cors( { origin: "*" }));
+
 // if (NODE_ENV === "production") {
 //   app.use(limiter);
 //   app.use(cors({ origin: URL }));
